24350e0a4ad031cca38718ad817c2fcf4530473f
ShuWang <wangshu_cn@hotmail.com>
2012-02-09 06:44:05 +0000

    Committing simple web server m3

---
+++ web_server_m2.py
@@ -0,0 +1,28 @@
+import socket
+import os
+
+host = ''
+port = 8080
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+sock.bind((host,port))
+sock.listen(1)
+
+while 1:
+        csock,caddr = sock.accept()
+        cfile = csock.makefile('rw',0)
+
+        # Protocol exchange - read request
+
+        while 1:
+                line = cfile.readline().strip()
+                if line == '':
+                        cfile.write("HTTP/1.0 200 OK\n\n")
+                        cfile.write("<head><title>Shu Wang's Python Test Web Server</title></head>")
+                        cfile.write("<h1>Shu Wang's Python Test Web Server</h1>")
+
+                        cfile.close()
+                        csock.close()
+                        break
+
